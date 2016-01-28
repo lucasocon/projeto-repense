b0471321bfc9d497f4a0bbd1001914cd4ad612d6
Lucas Ocon <lucas_masterclas@hotmail.com>
2016-01-27 21:09:18 -0300

    Add views and js validation

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Student, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Student, type: :model do
+  describe 'creation' do
+    it 'succeeds with all fields' do
+      expect(Student.new(name: 'Lucas', register_number: '123123')).to be_valid
+    end
+  end
 end
